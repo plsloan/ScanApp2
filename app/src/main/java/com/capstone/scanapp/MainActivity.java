45018c32d3d42a5ce9ee465615a8d4210910b45c
plsloan <phillipsloan24@gmail.com>
2018-04-28 16:10:42 -0400

    Added all views
    Views
    
    Main
    Enroll
    Verify
    Check Print
    Connect Reader

--- app/src/main/java/com/capstone/scanapp/MainActivity.java
+++ app/src/main/java/com/capstone/scanapp/MainActivity.java
@@ -1,34 +1,80 @@
 package com.capstone.scanapp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.digitalpersona.uareu.ReaderCollection;
+import com.digitalpersona.uareu.UareUException;
+
+
 public class MainActivity extends AppCompatActivity {
 
     boolean readerConnected = false;
+    private ReaderCollection readers;
+    private String m_deviceName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//         check for fingerprint reader
     }
 
     // click enroll button
     public void clickEnroll(View view) {
-        Intent intent = new Intent(this, EnrollActivity.class);
+        Intent enroll = new Intent(this, EnrollActivity.class);
+        Intent noConnection= new Intent(this, NoConnectionActivity.class);
 
-//        if (readerConnected) {}
-        startActivity(intent);
+//        // check for fingerprint reader
+//        try
+//        {
+//            Context applContext = getApplicationContext();
+//            Globals inst = Globals.getInstance();
+//            readers = inst.getReaders(applContext);
+//        } catch (UareUException e)
+//        {
+//            onBackPressed();
+//        }
+//
+//        if (readers.size() == 0)
+//            startActivity(noConnection);
+//        else
+        startActivity(enroll);
     }
 
     // click verify button
     public void clickVerify(View view) {
-        Intent intent = new Intent(this, EnrollActivity.class);
+        Intent verify = new Intent(this, VerifyActivity.class);
+        Intent noConnection= new Intent(this, NoConnectionActivity.class);
 
 //        if (readerConnected) {}
-        startActivity(intent);
+        startActivity(verify);
+    }
+
+    public void clickCheck(View view) {
+        Intent check = new Intent(this, CheckActivity.class);
+        Intent noConnection= new Intent(this, NoConnectionActivity.class);
+
+//        if (readerConnected) {}
+        startActivity(check);
+    }
+
+    public void clickConnect(View view) {
+        Intent connect = new Intent(this, ConnectActivity.class);
+        Intent noConnection= new Intent(this, NoConnectionActivity.class);
+
+//        if (readerConnected) {}
+        startActivity(connect);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent i = new Intent();
+        setResult(Activity.RESULT_OK, i);
+        finish();
     }
 }
