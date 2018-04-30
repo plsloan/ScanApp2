566e76ef1ae89adda11afef1e9d84c3481f1b9e1
plsloan <phillipsloan24@gmail.com>
2018-04-29 23:32:09 -0400

    Added Toolbar for added customization

--- app/src/main/java/com/capstone/scanapp/MainActivity.java
+++ app/src/main/java/com/capstone/scanapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.digitalpersona.uareu.ReaderCollection;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar Header = (Toolbar) findViewById(R.id.header);
+//        setSupportActionBar(Header);
     }
 
     // click enroll button
